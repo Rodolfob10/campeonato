8eb8684f9cfc7eed1472ddcebbbdf0a484cd778a
Rodolfob10 <rodolfobsilva10@gmail.com>
2019-03-08 15:05:32 -0300

    Reajustes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,22 +1,10 @@
-create database campeonato;
+insert into partida(id,time1,time2,local,campeonato_id) values (100,'Botafogo','Flamengo','Nilton Santos',1);
+insert into partida(id,time1,time2,local,campeonato_id) values (100,'Vasco','Flamengo','Maracanã',2);
 
-use campeonato;
+insert into campeonato (id,nome,fase) values (1,'carioca','grupos');
+insert into campeonato (id,nome,fase) values (2,'brasileiro','grupos');
 
-CREATE TABLE `partida`(
-	`id`int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `time1`varchar(20),
-    `time2`varchar(20),
-    `local`varchar(20),
-	`campeonato_id` int(11) NOT NULL,
-    KEY `campeonato_id`(`campeonato_id`),
-	CONSTRAINT `emprestimo_ibfk_1` FOREIGN KEY (`campeonato_id`) REFERENCES `campeonato` (`id`))
-    ;
+
 
     
 
-
-create table `campeonato`(
-	`id` int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`nome` varchar(30) NOT NULL,
-    `fase` varchar(20) 
-    );
